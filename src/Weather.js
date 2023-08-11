40331b82d05bf9f5bef55524fd07b8b5c0721af2
Elena Kononchuk <eva.kononchuk@gmail.com>
2023-08-11 21:45:32 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -5,17 +5,41 @@ import axios from "axios";
 import "./styles.css";
 
 export default function Weather(props) {
-  let [city, setCity] = useState("");
-  let [message, setMessage] = useState("");
+  let [city, setCity] = useState(props.city);
+  //let [message, setMessage] = useState("");
   let [temperature, setTemperature] = useState(null);
   let [description, setDescription] = useState(null);
   let [humidity, setHumidity] = useState(null);
   let [wind, setWind] = useState(null);
+  let [loaded, setLoaded] = useState(false);
+
+  function showForecast(response) {
+    setLoaded(true);
+    setTemperature(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+  }
 
   function showWeather(event) {
     event.preventDefault();
-    setMessage(
+    let apiKey = "96771e971243152d6b8948878c26adde";
+    let unit = "metric";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(url).then(showForecast);
+  }
+
+  function getCity(event) {
+    setCity(event.target.value);
+  }
+
+  if (loaded) {
+    return (
       <div className="App">
+        <form onSubmit={showWeather}>
+          <input type="search" onChange={getCity}></input>
+          <input type="submit" value="Search"></input>
+        </form>
         <div className="forecast">
           <div>Temperature: {temperature}°C</div>
           <div>Description: {description}</div>
@@ -24,31 +48,14 @@ export default function Weather(props) {
         </div>
       </div>
     );
+  } else {
+    return (
+      <div className="App">
+        <form onSubmit={showWeather}>
+          <input type="search" onChange={getCity}></input>
+          <input type="submit" value="Search"></input>
+        </form>
+      </div>
+    );
   }
-
-  function getCity(event) {
-    setCity(event.target.value);
-  }
-
-  function showForecast(response) {
-    setTemperature(Math.round(response.data.main.temp));
-    setDescription(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWind(response.data.wind.speed);
-  }
-
-  let apiKey = "96771e971243152d6b8948878c26adde";
-  let unit = "metric";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
-  axios.get(url).then(showForecast);
-
-  return (
-    <div className="App">
-      <form onSubmit={showWeather}>
-        <input type="search" onChange={getCity}></input>
-        <input type="submit" value="Search"></input>
-      </form>
-      <h2>{message}</h2>
-    </div>
-  );
 }
